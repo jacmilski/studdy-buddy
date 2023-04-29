83f5da0e4da9156e898a7d6f4d26a2a91556d482
jacmilski <jacmilski@gmail.com>
2023-04-29 22:20:08 +0200

    created new components: UsersList and UsersListItem

---
+++ src/views/Root.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import UsersList from 'components/UsersList/UsersList';
+import 'views/App.css';
+
+function Root() {
+  return (
+    <>
+      <UsersList />
+    </>
+  );
+}
+
+export default Root;
