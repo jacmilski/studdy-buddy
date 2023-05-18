9f3e845d5f9031412563c489ff4543f5cf7f1024
jacmilski <jacmilski@gmail.com>
2023-05-19 01:10:49 +0200

    Added SearchProvider and searching functionality

--- src/components/Organisms/SearchBar/SearchBar.js
+++ src/components/Organisms/SearchBar/SearchBar.js
@@ -1,8 +1,40 @@
-import React from 'react';
+import React, { useState, useContext } from 'react';
+import styled from 'styled-components';
 import { Input } from 'components/Atoms/Input/Input';
+import { SearchContext } from 'Providers/SearchProvider';
+import { ViewsWrapper } from 'components/Organisms/ViewsWrapperStyles/ViewsWrapper.styles';
+import { Wrapper } from 'components/Molecules/UsersListItem/UsersListItem.styles';
 import { SearchBarWrapper, StatusInfo } from './SearchBar.styles';
 
+const SearchingBox = styled.div`
+  display: flex;
+  flex-direction: column;
+`;
+
+const SearchingListWrapper = styled(ViewsWrapper)`
+  position: absolute;
+  top: 40px;
+  left: 320px;
+  border-radius: 0 15px 15px 15px;
+  max-width: 350px;
+  padding: 20px 30px;
+  z-index: 10;
+`;
+
+const SearchingListItem = styled(Wrapper)`
+  height: 40px;
+`;
+
 function SearchBar() {
+  const [sentence, setSentence] = useState('');
+
+  const { filterUsers, filteredUsers } = useContext(SearchContext);
+
+  const handleChange = (e) => {
+    setSentence(e.target.value);
+    filterUsers(e.target.value);
+  };
+
   return (
     <SearchBarWrapper>
       <StatusInfo>
@@ -11,7 +43,16 @@ function SearchBar() {
           <strong>Teacher</strong>
         </p>
       </StatusInfo>
-      <Input />
+      <SearchingBox>
+        <Input value={sentence} onChange={handleChange} />
+        {filteredUsers.length > 0 && sentence.length > 0 ? (
+          <SearchingListWrapper>
+            {filteredUsers.map((user) => (
+              <SearchingListItem>{user.name}</SearchingListItem>
+            ))}
+          </SearchingListWrapper>
+        ) : null}
+      </SearchingBox>
     </SearchBarWrapper>
   );
 }
